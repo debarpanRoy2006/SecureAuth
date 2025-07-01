34544c34e2e5683d2f16849aa949399ec0885192
debarpanRoy2006 <debarpanroy984@gmail.com>
2025-07-01 21:59:04 +0530

    Creating Superuser

--- build.sh
+++ build.sh
@@ -4,10 +4,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
-if [ "$CREATE_SUPERUSER" = "true" ]; then
-  echo "Creating superuser..."
-  python manage.py createsuperuser \
-    --noinput \
-    --username "$DJANGO_SUPERUSER_USERNAME" \
-    --email "$DJANGO_SUPERUSER_EMAIL"
-fi
+python manage.py createsu
