986a47e0faf753879e30d0cbe9fa13a3a53ca168
debarpanRoy2006 <debarpanroy984@gmail.com>
2025-07-01 22:09:45 +0530

    Creating Superuser

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 set -o errexit
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
+echo "Running custom superuser creation..."
 python manage.py createsu
