804ac1fc4d9ff073408b4914abdb1722c2008ce7
debarpanRoy2006 <debarpanroy984@gmail.com>
2025-07-01 22:25:10 +0530

    Creating Superuser

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,8 +1,11 @@
+# auth_app/admin.py
 
-
-# Register your models here.
-# auth_project/auth_app/admin.py
 from django.contrib import admin
 from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(User)
+# Unregister the original
+admin.site.unregister(User)
+
+# Register again (optionally with your custom UserAdmin subclass)
+admin.site.register(User, UserAdmin)
